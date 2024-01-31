5e9e19dfd89a6e105c9fbfe88de197281b94c4a3
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2024-01-31 08:04:43 +0600

    get, post, and create method implemented

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -37,42 +37,4 @@ router.post('/', (req, res) => {
         .catch(err => console.log(err));
 });
 
-// Update a task by ID
-router.put('/:id', (req, res) => {
-    if (ObjectId.isValid(req.params.id) == false)
-        res.status(400).json({
-            error: 'Given object ID is not valid.'
-        });
-    else
-        Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
-            .then(data => {
-                if (data)
-                    res.send(data);
-                else
-                    res.status(404).json({
-                        error: 'No record with given _id:' + req.params.id
-                    });
-            })
-            .catch(err => console.log(err));
-});
-
-// Delete a task by ID
-router.delete('/:id', (req, res) => {
-    if (ObjectId.isValid(req.params.id) == false)
-        res.status(400).json({
-            error: 'Given object ID is not valid.'
-        });
-    else
-        Task.deleteOne({ _id: req.params.id })
-            .then(data => {
-                if (data.deletedCount > 0)
-                    res.send(data);
-                else
-                    res.status(404).json({
-                        error: 'No record with given _id:' + req.params.id
-                    });
-            })
-            .catch(err => console.log(err));
-});
-
 module.exports = router;
